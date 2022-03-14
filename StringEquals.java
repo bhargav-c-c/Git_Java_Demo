c20a6cd704a16f30f8e2351b94592f5854aac4cc
bhargav-c-c <bhargav.chitragar@gmail.com>
2022-03-14 12:11:04 +0530

    first string file

---
+++ StringEquals.java
@@ -0,0 +1,12 @@
+package com.te.javabasics.string;
+
+public class StringEquals {
+	public static void main(String[] args) {
+		String s1 = "java";
+		String s2 = "java";
+		System.out.println(s1.equals(s2));
+		System.out.println(s1.equalsIgnoreCase(s2));
+
+	}
+
+}
